0689946dc0a934d269b2d3be5103356a1615361f
EugeneSemivolos <semivolos.evgen1@gmail.com>
2024-08-22 23:51:32 +0300

    implemented random quotes

--- js/script.js
+++ js/script.js
@@ -19,4 +19,24 @@ window.addEventListener('DOMContentLoaded', () => {
     function updateLikeBtn() {
         likeBtn.src = `images/like/${getLike() ? 'heart' : 'empty_heart'}.png`;
     }
+
+    //quotes
+    const quoteData = [
+        ["«Уранці, коли пробуджуєшся, спитай себе: „Що я маю зробити?“ Увечері, перше ніж заснути: „Що я зробив?“»", "Піфагор"],
+        ["«Не турбуйтеся, якщо щось не працює. Якби все працювало, вас звільнили б»",                                "Закон Мошера програмної інженерії"],
+        ["«Завдяки істинному знанню ти будеш набагато сміливішим і досконалішим у кожній роботі, ніж без нього»",    "А. Дюрер"],
+        ["«Велике мистецтво багато чого навчитися — це братися відразу за небагато»",                                "Джон Локк"],
+        ["«Вся гордість вчителя в учнях, у зростанні посіяного ним насіння»",                                        "Д. Менделєєв"],
+    ]
+    const rand = Math.floor(Math.random()*quoteData.length);
+
+    const quote = document.createElement('p');
+    quote.innerHTML = quoteData[rand][0];
+
+    const author = document.createElement('p');
+    author.innerHTML = quoteData[rand][1];
+
+    document.querySelector('.favorite-quote').append(quote, author);
+
+
 });
